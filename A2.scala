c27bc26e7567d2f9fb40c02b0a67df457255b1c5
Jayant <jayantjnp@gmail.com>
2017-11-29 00:51:13 -0500

    stage 2

--- A2.scala
+++ A2.scala
@@ -1,24 +1,45 @@
-import org.apache.spark.graphx.GraphLoader
+// import org.apache.spark.graphx.GraphLoader
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
 
 object A2 {
- def main(args: Array[String]) {
- 	val conf = new SparkConf().setAppName("Connected Components using Graphx")
-    val sc = new SparkContext(conf)
-    if (args.length != 2) {//arguments needed
-        println("Two parameters are required, input file and output folder name")
-        return
-    }
-    val input = args(0)
-    val output = args(1)
-	val graph = GraphLoader.edgeListFile(sc, input)//lading the graph from the input file
-	//Finding the connected components, calling the inbuilt function
-	val cc = graph.connectedComponents().vertices
-	val disp = cc.map(x => x._1 + " " + x._2)//removing brackets and comma from source and destinations vertices, and adding space in between them
-	val outputgraph = disp.collect().mkString("\n")// making RDD come into existence
-	disp.saveAsTextFile(output)//saving the RDD to a output file
-	// println(outputgraph)//debug
-   }
+	 def main(args: Array[String]) {
+	 	val conf = new SparkConf().setAppName("Connected Components using Graphx")
+	    val sc = new SparkContext(conf)
+	    if (args.length != 2) {//arguments needed
+	        println("Two parameters are required, input file and output folder name")
+	        return
+	    }
+	    val input = args(0)
+	    val output = args(1)
+		val graph = sc.textFile(input)//reading the graph
+		val verticesRDD = graph.map(line=>{
+			val vertices = line.split(" ")
+			(vertices(0), vertices(1))
+		})
+		val disp = verticesRDD.map(x => {
+			x._1 + " " + x._2
+		})//original graph
+		// val largeStar = verticesRDD.map(ver => {
+		// 	(ver._1,ver._2)
+
+		// })
+		//////////////////large star/////////////////
+		//Map
+		val expandVer = verticesRDD.flatMap(vertices => {//emitting the (u,v) and (v,u)
+			(vertices._1, vertices._2)
+			if (vertices._1 == vertices._2)
+				((vertices._1, vertices._1)).tolist
+			else
+				((vertices._1, vertices._2), (vertices._2, vertices._1)).tolist
+		})
+		//reduce
+		// val neighbors
+		// val minVertex = argMin
+		println(expandVer.collect().mkString("\n"))
+	}
+	def argMin(nodes: List[Long]): Long = {
+		nodes.min(Ordering.by((node: Long) => node))
+	}
 }
--- build.sbt
+++ build.sbt
@@ -4,5 +4,5 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-libraryDependencies += "org.apache.spark" %% "spark-graphx" % "2.2.0";
+// libraryDependencies += "org.apache.spark" %% "spark-graphx" % "2.2.0";
 libraryDependencies += "org.apache.spark" %% "spark-core" % "1.5.2"
